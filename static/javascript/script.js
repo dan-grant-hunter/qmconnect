d14dfefa3956afd74f0a20badaa4b1c5fb2aaf7a
Catalin Pit <catalinpit@gmail.com>
2019-02-28 19:09:16 +0000

    .

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,6 +1,7 @@
 $(function() {
   console.log('Script loaded')
 
+  // retrieve the conversation
   $('.conversation-partner').on('click', function() {
     // this refers to the a tag
     // this.id retrieve the id from the a tag
@@ -37,6 +38,7 @@ function showMessage(conversation_id, receiver) {
       console.log('success')
       $('.messages').html('')
       $('.messages').append(response + "</br>")
+      // attach the receiver and conversation IDs to the webpage so they can be retrieved by the other function - sendMessage
       $('.messages').append('<span style="display:none;" id="receiver-span">' + receiver + '</span>')
       $('.messages').append('<span style="display:none;" id="conversation-span">' + conversation_id + '</span>')
     },
@@ -45,7 +47,6 @@ function showMessage(conversation_id, receiver) {
     }
   });
 }
-
 
 /*
   This function allows users to send messages
@@ -58,15 +59,16 @@ function sendMessage(receiver_id) {
       'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
       'text': $('input[name=text]').val(),
       'request.user': $('.request-user').attr('id'),
+      // send the conversation id so the new message can be attached to the proper conversation
       'conversation_id': $('#conversation-span').text()
     },
     success: function(response) {
       console.log("Message sent successfully!")
       $('.messages').append(response['sender'] + " to " + response['receiver'] + ": " + response['text'])
+      // clear the input after sending the message
       $('input[name=text]').val('')
     },
     error: function(xhr) {
-      console.log($('.request-user').attr('id'))
       console.log(xhr)
     }
   });
